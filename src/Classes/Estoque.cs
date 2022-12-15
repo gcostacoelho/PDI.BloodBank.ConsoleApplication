63139fee7494cedd88a3a67e6827d33bae29e9a0
gcostacoelho <gcostacoelho2003@gmail.com>
2022-12-15 09:30:36 -0300

    create-estoque

---
+++ src/Classes/Estoque.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PDI.BloodBank.ConsoleApplication.src.Classes
+{
+    public class Estoque
+    {
+        
+    }
+}
